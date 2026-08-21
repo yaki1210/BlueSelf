d6c9764b54763b8400f3a409aa23f24da74c4a62
yaki1210 <145995489+yaki1210@users.noreply.github.com>
2026-08-21 10:19:27 +0800

    自绘窗口标题栏并主题化日志展开按钮

--- windows/FileTransferApp.WinUI/UI/MainWindow.xaml.cs
+++ windows/FileTransferApp.WinUI/UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 using FileTransferApp.WinUI.ViewModels;
 
 namespace FileTransferApp.WinUI.UI;
@@ -9,5 +10,38 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = MainViewModel.Instance;
+    }
+
+    // 标题栏拖拽移动窗口
+    private void OnHeaderMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+            DragMove();
+    }
+
+    private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void OnMaximizeButtonClick(object sender, RoutedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+            MaximizeButton.Content = "\uE922";
+            MaximizeButton.ToolTip = "最大化";
+        }
+        else
+        {
+            WindowState = WindowState.Maximized;
+            MaximizeButton.Content = "\uE923";
+            MaximizeButton.ToolTip = "向下还原";
+        }
+    }
+
+    private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
